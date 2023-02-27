54674c6575bca975ccd185ea9391970ec9cc04a8
Peter Justine Ramos <dchamp16@gmail.com>
2023-02-27 15:56:55 +0000

    second remove this because first commit dint work from codesandbox

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
         {/* main content */}
         <div className="body">
           <h1>Remove this</h1>
+          <h2>Second remove this</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
             accusantium cumque sunt, animi ipsum mollitia vero nobis optio
