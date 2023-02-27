59cbd952091b541f1324573f09aa89d9c80ecfaf
dchamp16 <dchamp16@gmail.com>
2023-02-26 21:58:43 -0700

    added a footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,17 +5,125 @@ const Footer = () => {
 
   return (
     <div>
-      <footer>
-        <h1>Footer title</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem, distinctio iste! Ea, ipsum! Nulla labore ea sapiente
-          veniam necessitatibus inventore pariatur nam nobis esse eaque ab natus
-          neque at beatae harum impedit quidem nostrum totam est officiis nihil,
-          voluptatem ratione, quam quisquam! Earum labore maxime fugit omnis
-          reprehenderit aut nobis?
-        </p>
-        <p>Copyright &copy; {copyrightDate.getFullYear()}</p>
+      <footer class="text-center text-lg-start bg-white text-muted">
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+          <div class="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
+          </div>
+
+          <div>
+            <a href="" class="me-4 link-secondary">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+              <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+              <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+              <i class="fab fa-github"></i>
+            </a>
+          </div>
+        </section>
+
+        <section class="">
+          <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">
+                  <i class="fas fa-gem me-3 text-secondary"></i>Company name
+                </h6>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Quaerat quos ipsam nesciunt accusamus minus vitae quia facilis
+                  totam quasi soluta!
+                </p>
+              </div>
+
+              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Angular
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    React
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Vue
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Laravel
+                  </a>
+                </p>
+              </div>
+
+              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Pricing
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Settings
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Orders
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Help
+                  </a>
+                </p>
+              </div>
+
+              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                <p>
+                  <i class="fas fa-home me-3 text-secondary"></i> New York, NY
+                  10012, US
+                </p>
+                <p>
+                  <i class="fas fa-envelope me-3 text-secondary"></i>
+                  info@example.com
+                </p>
+                <p>
+                  <i class="fas fa-phone me-3 text-secondary"></i> + 01 234 567
+                  88
+                </p>
+                <p>
+                  <i class="fas fa-print me-3 text-secondary"></i> + 01 234 567
+                  89
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div class="text-center p-4">
+          © 2021 Copyright:
+          <a class="text-reset fw-bold" href="https://mdbootstrap.com/">
+            CHANGE THIS
+          </a>
+        </div>
       </footer>
     </div>
   );
